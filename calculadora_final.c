602dc9978975b50600a50d7a9663ca6f4358b459
Aravz8 <miguelarauzvillarreal@gmail.com>
2026-08-02 11:12:46 -0500

    Fix DrawRectangleRoundedLines function calls

--- calculadora_final.c
+++ calculadora_final.c
@@ -160,7 +160,7 @@ int botonConLetra(Rectangle rect, char letra, const char *label, int seleccionad
     Color colorTxt = seleccionado ? COLOR_ACENTO_TXT : COLOR_TEXTO;
 
     DrawRectangleRounded(rect, 0.3f, 8, fondo);
-    if (!seleccionado) DrawRectangleRoundedLines(rect, 0.3f, 8, 2.0f, COLOR_BORDE);
+    if (!seleccionado) DrawRectangleRoundedLines(rect, 0.3f, 8, COLOR_BORDE);
 
     Rectangle cajaLetra = { rect.x + 10, rect.y + rect.height/2 - 14, 28, 28 };
     Color fondoLetra = seleccionado ? (Color){255,255,255,90} : COLOR_TARJETA;
@@ -181,7 +181,7 @@ int boton(Rectangle rect, const char *label, int seleccionado) {
     Color colorTxt = seleccionado ? COLOR_ACENTO_TXT : COLOR_TEXTO;
 
     DrawRectangleRounded(rect, 0.35f, 8, fondo);
-    if (!seleccionado) DrawRectangleRoundedLines(rect, 0.35f, 8, 2.0f, COLOR_BORDE);
+    if (!seleccionado) DrawRectangleRoundedLines(rect, 0.35f, 8, COLOR_BORDE);
     texto(label, (int)rect.x + 14, (int)(rect.y + rect.height/2 - 9), 16, colorTxt);
 
     return FuePresionadoEnRec(rect);
@@ -194,11 +194,11 @@ void casilla(Rectangle rect, const char *label, int *marcado) {
 
     Color fondo = *marcado ? COLOR_ACENTO : (Color){255,255,255,255};
     DrawRectangleRounded(rect, 0.3f, 8, fondo);
-    if (!*marcado) DrawRectangleRoundedLines(rect, 0.3f, 8, 2.0f, COLOR_BORDE);
+    if (!*marcado) DrawRectangleRoundedLines(rect, 0.3f, 8, COLOR_BORDE);
 
     Rectangle caja = { rect.x + 12, rect.y + rect.height/2 - 9, 18, 18 };
     DrawRectangleRounded(caja, 0.3f, 6, (Color){255,255,255,255});
-    DrawRectangleRoundedLines(caja, 0.3f, 6, 2.0f, COLOR_BORDE);
+    DrawRectangleRoundedLines(caja, 0.3f, 6, COLOR_BORDE);
     if (*marcado) {
         DrawLineEx((Vector2){caja.x+3, caja.y+9}, (Vector2){caja.x+7, caja.y+14}, 2.5f, COLOR_ACENTO_TXT);
         DrawLineEx((Vector2){caja.x+7, caja.y+14}, (Vector2){caja.x+15, caja.y+4}, 2.5f, COLOR_ACENTO_TXT);
@@ -376,7 +376,7 @@ int main(void) {
             texto("Tu nombre:", 40, 290, 13, COLOR_TEXTO_TENUE);
             Rectangle campoNombre = { 40, 310, ANCHO - 80, 44 };
             DrawRectangleRounded(campoNombre, 0.25f, 8, (Color){255,255,255,255});
-            DrawRectangleRoundedLines(campoNombre, 0.25f, 8, 2.0f, COLOR_BORDE);
+            DrawRectangleRoundedLines(campoNombre, 0.25f, 8, COLOR_BORDE);
 
             if (letrasNombre == 0) {
                 texto("Escribe tu nombre...", (int)campoNombre.x + 14, (int)campoNombre.y + 13, 14, COLOR_TEXTO_TENUE);
